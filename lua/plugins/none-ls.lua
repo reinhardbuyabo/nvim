08beb302cfa8068c13a8a35f0ecaa9507eb8ac99
reinhardbuyabo <reinhard.buyabo@strathmore.edu>
2025-08-12 00:40:03 +0300

    Debugging

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,26 @@
+return {
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim", -- eslint_d
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        -- LUA
+        null_ls.builtins.formatting.stylua,
+        -- JAVASCRIPT
+        null_ls.builtins.formatting.prettier,
+        require("none-ls.diagnostics.eslint_d"),
+        -- C++
+        require("none-ls.diagnostics.cpplint"),
+        null_ls.builtins.formatting.clang_format,
+        -- PYTHON
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+      },
+    })
+
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
+}
