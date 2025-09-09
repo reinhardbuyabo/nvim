cf4cc439390c24eec4316feb7daf12cabe5c74a7
reinhardbuyabo <buyabor@gmail.com>
2025-09-09 22:05:00 +0300

    refactor: minor changes

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -31,7 +31,7 @@ return {
             -- Global LSP keymaps
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set({ "n", "v" }, "<leader>vca", vim.lsp.buf.code_action, {})
             vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, {})
             vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, {})
             vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, {})
